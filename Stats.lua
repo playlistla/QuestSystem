e8f96eba8c32904d213e1f12542b685d0886e7a0
playlistla <piaylistla222@gmail.com>
2025-09-14 09:13:07 -0500

    Add files via upload

---
+++ Stats.lua
@@ -0,0 +1,14 @@
+local Players = game:GetService("Players")
+
+local function onPlayerAdded(player)
+    local leaderstats = Instance.new("Folder")
+    leaderstats.Name = "leaderstats"
+    leaderstats.Parent = player
+
+    local coins = Instance.new("IntValue")
+    coins.Name = "Coins"
+    coins.Value = 0
+    coins.Parent = leaderstats
+end
+
+Players.PlayerAdded:Connect(onPlayerAdded)
